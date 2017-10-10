1183690faacfcc20da697d5fc1d3ac2c45cf357b
quocbao_home <quocbao.tn@gmail.com>
2017-10-10 07:00:59 +0700

    deploy customupload image autoSave()

---
+++ assets/plugins/ckeditor/plugins/customupload/plugin.js
@@ -0,0 +1,82 @@
+CKEDITOR.plugins.add('customupload', {
+
+  icons: 'customupload',
+
+  init: function (editor) {
+    editor.addCommand('uploadImage', new CKEDITOR.dialogCommand('uploadImageDialog'));
+    editor.ui.addButton('uploadImage', {
+      label: 'upload image',
+      command: 'uploadImage',
+      toolbar: 'insert',
+      icon: this.path + 'icons/customupload.png'
+    });
+
+    CKEDITOR.dialog.add('uploadImageDialog', function (api) {
+      return {
+        title: 'UploadImage Dialog',
+        minWidth: 400,
+        minHeight: 200,
+        contents: [{
+            id: 'tab-basic',
+            label: 'Upload image',
+            elements: [{
+              type: 'file',
+              id: 'uploadImage',
+              label: 'upload image',
+              validate: CKEDITOR.dialog.validate.notEmpty("uploadImage field cannot be empty."),
+              commit: function () {
+                alert('commit')
+              }
+            }]
+          }
+
+        ],
+        onShow: function () {
+          var dialog = this;
+          var el = dialog.getContentElement('tab-basic', 'uploadImage').getElement()
+          var iframe = el.$.querySelector('iframe')
+          var file = $(iframe).contents().find('input')[0]
+          file.setAttribute('accept', 'image/*')
+          file.addEventListener("change", () => {
+            var uploadUrl = this._.editor.config.uploadUrl
+            file.setAttribute('disabled', 'disabled')
+            var formData = new FormData();
+            formData.append("upload", file.files[0]);
+            fetch(uploadUrl, {
+              method: 'POST',
+              body: formData
+            }).then(function (response) {
+              return response.json()
+            }).then(res => {
+              rec = res.result.files.upload[0]
+              var img = document.createElement('img')
+              console.log('this', this, dialog)
+              var url = this._.editor.config.uploadUrl.replace('upload', 'download') + '/' + rec.name
+              img.setAttribute('src', url)
+              img.style.height = '200px'
+              img.style.width = '200px'
+
+              if (el.$.querySelector('img')) {
+                el.$.replaceChild(img, el.$.querySelector('img'))
+              } else {
+                el.$.insertBefore(img, el.$.lastChild);
+              }
+
+              file.removeAttribute('disabled', 'disabled')
+            }).catch(err => alert(err))
+          })
+        },
+        onOk: function () {
+          var dialog = this;
+          var el = dialog.getContentElement('tab-basic', 'uploadImage').getElement()
+          var iframe = el.$.querySelector('iframe')
+          var file = $(iframe).contents().find('input')[0]
+          var img = editor.document.createElement('img');
+          var url = this._.editor.config.uploadUrl.replace('upload', 'download') + '/' + file.files[0].name
+          img.setAttribute('src', url)
+          editor.insertElement(img);
+        }
+      };
+    });
+  }
+})
